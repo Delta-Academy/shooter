a966c8d0a9db37e2adba746a9bf5827d3e9d93ea
neuromantic99 <jrowlandm@gmail.com>
2022-09-13 15:47:30 +0100

    changed install to include asset subfolder and pngs inside

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="delta_shooter",
     version="0.1",
-    packages=["delta_shooter", "delta_shooter.assets"],
+    packages=["delta_shooter"],
+    include_package_data=True,
     description="DESCRIPTION OF GAME",
     author="AUTHOR",
     author_email="AUTHOR-EMAIL",
