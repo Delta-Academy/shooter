0b2be31d63a6d0b78d650abc2a8bbb28cd18d853
neuromantic99 <jrowlandm@gmail.com>
2022-09-13 15:37:21 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="delta_shooter",
     version="0.1",
-    packages=["delta_shooter"],
+    packages=["delta_shooter", "delta_shooter.assets"],
     description="DESCRIPTION OF GAME",
     author="AUTHOR",
     author_email="AUTHOR-EMAIL",
